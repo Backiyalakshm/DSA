abd006fc162d0117c2a3bac04220829d774297dd
Packiyalakshmi M <168925796+Backiyalakshm@users.noreply.github.com>
2025-06-05 14:35:30 +0530

    Time: 0 ms (100.00%) | Memory: 45 MB (17.67%) - LeetSync

--- 328-odd-even-linked-list/odd-even-linked-list.java
+++ 328-odd-even-linked-list/odd-even-linked-list.java
@@ -1,37 +1,20 @@
 class Solution {
     public ListNode oddEvenList(ListNode head) {
-        if (head == null) return null;
+        if (head == null || head.next == null) return head;
 
-        // Step 1: Count the total number of nodes
-        ListNode temp = head;
-        int cnt = 0; 
-        while (temp != null) {
-            cnt++;
-            temp = temp.next;
-        }
+        ListNode odd = head;
+        ListNode even = head.next;
+        ListNode evenHead = even;
 
-        // Step 2: Store odd-positioned node values
-        int[] arr = new int[cnt];
-        temp = head;
-        int index = 0;
-        while (temp != null) {
-            arr[index++] = temp.val;
-            temp = temp.next;
+        while (even != null && even.next != null) {
+            odd.next = even.next;
+            odd = odd.next;
+
+            even.next = odd.next;
+            even = even.next;
         }
 
-        // Step 3: Write odd indices first, then even
-        temp = head;
-        int i = 0;
-        // Write values at odd indices (0-based: index 0, 2, 4,...)
-        for (int j = 0; j < cnt; j += 2) {
-            temp.val = arr[j];
-            temp = temp.next;
-        }
-        // Write values at even indices (0-based: index 1, 3, 5,...)
-        for (int j = 1; j < cnt; j += 2) {
-            temp.val = arr[j];
-            temp = temp.next;
-        }
+        odd.next = evenHead;
 
         return head;
     }
