10bbcb52af78f22b3e1de9ac6bc4eb5447875777
Packiyalakshmi M <168925796+Backiyalakshm@users.noreply.github.com>
2026-02-18 12:10:00 +0530

    Time: 3 ms (12.22%) | Memory: 43.3 MB (69.52%) - LeetSync

--- 3396-valid-word/valid-word.java
+++ 3396-valid-word/valid-word.java
@@ -8,11 +8,13 @@ class Solution {
             return true;
         }
         return false;
+
+        
     }
     public boolean atleastVowel(String word){
         boolean flag = false;
         for(int i =0; i < word.length(); i++){
-            if( Character.isLetterOrDigit(word.charAt(i)) && Character.isLetter(word.charAt(i))  && word.charAt(i) == 'A' || word.charAt(i) == 'E' || word.charAt(i) == 'I' || word.charAt(i) == 'O' || word.charAt(i) == 'U'  )
+            if( Character.isLetterOrDigit(word.charAt(i)) && Character.isLetter(word.charAt(i))  && word.charAt(i) == 'A' || word.charAt(i) == 'E' || word.charAt(i) == 'I' || word.charAt(i) == 'O' || word.charAt(i) == 'U')
             {
                 flag = true;
             }
