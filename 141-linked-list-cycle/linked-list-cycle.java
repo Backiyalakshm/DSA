49b948ff03a305c3655dcf73a80023c36121a4d6
Packiyalakshmi M <168925796+Backiyalakshm@users.noreply.github.com>
2026-03-06 11:39:59 +0530

    Time: 0 ms (100.00%) | Memory: 46.8 MB (72.55%) - LeetSync

--- 141-linked-list-cycle/linked-list-cycle.java
+++ 141-linked-list-cycle/linked-list-cycle.java
@@ -17,7 +17,7 @@ ListNode slow = head;
 while(fast != null && fast.next != null) {
             fast= fast.next.next;
           slow= slow.next;
-        if (fast== slow) {
+        if (fast == slow) {
              return true;
 }
 }
