9d86a4af542ce78046147e6724e61d3d04080352
Packiyalakshmi M <168925796+Backiyalakshm@users.noreply.github.com>
2026-03-16 14:58:53 +0530

    Time: 5 ms (11.80%) | Memory: 42.1 MB (54.49%) - LeetSync

---
+++ 52-n-queens-ii/n-queens-ii.java
@@ -0,0 +1,44 @@
+class Solution {
+    public int totalNQueens(int n) {
+   
+        List<List<String>> list = new ArrayList<>();
+        char[][] board = new char[n][n];
+        for(int i = 0 ;i < n; i++){
+            Arrays.fill(board[i],'.');
+        }
+        place(board,0,list);
+        return list.size();
+    }
+    public void place(char[][] board , int row, List<List<String>> res){
+        if(row == board.length){
+            List<String> list1 = new ArrayList<>();
+            for(int i =0; i < board.length; i++){
+                list1.add(new String(board[i]));
+            }
+            res.add(list1);
+        }
+        for(int col = 0; col < board.length; col++){
+            if(isSafe(board , row, col)){
+                board[row][col] = 'Q';
+                place(board , row +1, res);
+                board[row][col] = '.';
+            }
+        }
+    }
+    public boolean isSafe(char[][] board,int row, int col){
+        for(int i =0 ;i < row; i++){
+            if(board[i][col] == 'Q'){
+                return false;
+            }
+            for(int j = 0; j < board.length; j++){
+                if(board[i][j] == 'Q'){
+                    if(Math.abs(row - i) == Math.abs(col - j)){
+                        return false;
+                    }
+
+                }
+            }
+        }
+        return true;
+    }
+}
