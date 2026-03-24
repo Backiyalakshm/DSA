077a2e9b0c5308b3f818c0fd4e826d5996519576
Packiyalakshmi M <168925796+Backiyalakshm@users.noreply.github.com>
2026-03-24 14:40:33 +0530

    Time: 0 ms (100.00%) | Memory: 47.1 MB (20.79%) - LeetSync

--- 733-flood-fill/flood-fill.java
+++ 733-flood-fill/flood-fill.java
@@ -1,26 +1,23 @@
 class Solution {
-    private void dfs(int row, int col, int[][] ans, int[][] image, int newcol, int[] delrow,int[] delcol ,int ini)
-    {
-        ans[row][col] = newcol;
-        int n = image.length;
-        int m = image[0].length;
-        for(int i =0; i<4; i++)
-        {
-            int nrow = row+delrow[i];
-            int ncol = col+delcol[i];
-            if(nrow>=0 && nrow <n && ncol >=0 && ncol <m&& image[nrow][ncol] == ini&& ans[nrow][ncol]!= newcol)
-            {
-                dfs(nrow,ncol,ans,image,newcol,delrow,delcol,ini);
-            }
-        }
-
-    }
     public int[][] floodFill(int[][] image, int sr, int sc, int color) {
-        int ini = image[sr][sc];
-        int[][] ans =image;
-        int[] delrow = {-1,0,1,0};
-        int[] delcol = {0,1,0,-1};
-        dfs(sr,sc,ans,image,color,delrow,delcol,ini);
-        return ans;
+       
+        if(color != image[sr][sc])
+        {
+             int ori = image[sr][sc];
+             backtrack(image,sr,sc,ori,color);
+        }
+        return image;
+    }
+    public void backtrack(int[][] image, int sr, int sc,int org, int color){
+        if(sr < 0 || sr >= image.length || sc < 0 || sc >= image[0].length ||image[sr][sc] != org){
+            return;
+        }
+        image[sr][sc] = color;
+       backtrack(image,sr+1,sc,org,color);
+        backtrack(image,sr,sc+1,org,color);
+         backtrack(image,sr-1,sc,org,color);
+          backtrack(image,sr,sc-1,org,color);
+
+
     }
 }
